a203d295381e9e2b9b1f06a8caa01e859d419458
fursaweb <fursa.web@gmail.com>
2026-03-11 19:35:22 +0200

    Add register route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import express, { Request, Response } from "express";
 import { prisma } from "./prisma";
 import dotenv from "dotenv";
+import * as z from "zod";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+
+const Login = z.object({
+  email: z.email(),
+  password: z.string().min(6),
+});
 
 dotenv.config();
 
@@ -16,6 +25,34 @@ app.post("/users", async (req: Request, res: Response) => {
 
   console.log("Created user:", user);
   return res.status(201).json(user);
+});
+
+app.post("/auth/register", (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const result = Login.safeParse({ email, password });
+
+  if (!result.success) {
+    const errors = result.error.issues.map((error) => error.message);
+    return res.status(400).json({ error: errors });
+  } else {
+    bcrypt.hash(password, saltRounds, async function (err, hash) {
+      if (err) {
+        return res.status(400).json({ error: err });
+      } else {
+        try {
+          const user = await prisma.user.create({
+            data: { email, password: hash },
+          });
+
+          return res.status(200).json({ user });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+  }
+
+  // return res.status(200).json({ ok: true });
 });
 
 const PORT = process.env.PORT || 3000;
